20274702a37ce9f796079863552cafbf4dc9739e
Taiwo <ajiwunmi@gmail.com>
2023-08-29 14:59:47 +0100

    Update app.py
    updated the message

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 name = "Taiwo Aremu"
 os = "Window-OS"
-print(f"Hi! My name is {name} and I developed this app in {os}, but you can run it everywhere :)")
+print(f"Hi! My name is {name} and I developed this app in {os}, but you can run it everywhere. Thanks to docker:)")
